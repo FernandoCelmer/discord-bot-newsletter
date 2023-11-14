c538e4797cefa42daad3739222bfe98a2c997b5e
FernandoCelmer <email@fernandocelmer.com>
2023-11-14 04:13:01 +0000

    ⚙️ FEATURE: Workaround to send messages faster before the Lambda API response

--- discord_bot/bot_background_task.py
+++ discord_bot/bot_background_task.py
@@ -14,23 +14,25 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.tree.command(name="subscribe", description="Subscribe Channel")
 async def subscribe(interaction:discord.Interaction):
+    await interaction.response.send_message(
+        "**Subscription completed.**"
+    )
     request = ApiChannel()
-
     response = request.post(
         name=interaction.channel.name,
         code=str(interaction.channel_id)
     )
 
-    if is_ok(response=response):
-        await interaction.response.send_message(
-            "**Subscription completed.**"
-        )
+    # if is_ok(response=response):
+    #     await interaction.response.send_message(
+    #         "**Subscription completed.**"
+    #     )
 
     if not is_ok(response=response):
         if response.status_code == 409:
-            await interaction.response.send_message(
-                "**Channel subscribed again.**"
-            )
+            # await interaction.response.send_message(
+            #     "**Channel subscribed again.**"
+            # )
             response = request.patch(
                 code=str(interaction.channel_id),
                 status=True
@@ -39,22 +41,26 @@ async def subscribe(interaction:discord.Interaction):
 
 @bot.tree.command(name="unsubscribe", description="Unsubscribe Channel")
 async def unsubscribe(interaction:discord.Interaction):
-    request = ApiChannel()
+    await interaction.response.send_message(
+        "**Unsubscribe Channel.**"
+    )
 
+    request = ApiChannel()
     response = request.patch(
         code=str(interaction.channel_id)
     )
 
-    if is_ok(response=response):
-        await interaction.response.send_message(
-            "**Unsubscribe Channel.**"
-        )
-    
+    # if is_ok(response=response):
+    #     await interaction.response.send_message(
+    #         "**Unsubscribe Channel.**"
+    #     )
+
     if not is_ok(response=response):
         if response.status_code == 404:
-            await interaction.response.send_message(
-                "**Channel not found.**"
-            )
+            pass
+            # await interaction.response.send_message(
+            #     "**Channel not found.**"
+            # )
 
 
 @bot.event
